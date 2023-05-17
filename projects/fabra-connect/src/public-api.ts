ed7052a6270a1c2672ed5b82dcf8409d4d197888
Yu Li <ylilarry@gmail.com>
2023-05-17 19:45:41 -0400

    Port react-fabra-connect to an Angular service
    Added FabraConnectService to the library

--- projects/fabra-connect/src/public-api.ts
+++ projects/fabra-connect/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of fabra-connect
  */
 
-export * from './lib/fabra-connect.service';
-export * from './lib/fabra-connect.component';
-export * from './lib/fabra-connect.module';
+export * from "./lib/fabra-connect.component";
+export * from "./lib/fabra-connect.module";
+export * from "./lib/fabra-connect.service";
+export * from "./lib/types";
