255d7e3e73e273c44bd8a7e189ebe7f6be4e5feb
Yu Li <ylilarry@gmail.com>
2023-05-17 20:43:31 -0400

    Add generated library code

---
+++ projects/fabra-connect/src/lib/fabra-connect.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-fabra-connect',
+  template: `
+    <p>
+      fabra-connect works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FabraConnectComponent {
+
+}
